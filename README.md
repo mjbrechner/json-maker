8729135f33a5e5076141e743025cd5619de63f13
MJ Brechner <mjbrechner@gmail.com>
2023-02-19 11:39:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+JSON Maker by M.J. Brechner
+
+I created this app in order to help me create the JSON file for my Scots-English dictionary project at https://github.com/mjbrechner/scots-english.
+With this app, I can quickly enter the dictionary data and copy the resulting file to be added to the actual dictionary JSON file.
+
+© 2023 M.J. Brechner
---
+++ javascript/index.js
@@ -0,0 +1,100 @@
+'use strict';
+let totalOutput;
+
+if (typeof totalOutput !== "undefined") {
+    console.log("not undefined" + totalOutput);
+} else {
+    console.log("undefined" + totalOutput);
+}
+
+
+// Funtion to clear notification
+function clearNotification() {
+document.getElementById("notification").innerText = ``;
+}
+
+function addToJSON() {
+    let scotsInput = document.getElementById("scots").value.trim();
+    let englishInput = document.getElementById("english").value.trim();
+    let definitionInput = document.getElementById("definition").value.trim();
+    let posInput = document.getElementById("pos").value.trim();
+    let searchInput1 = document.getElementById("search-input1").value.trim();
+    let searchInput2 = document.getElementById("search-input2").value.trim();
+    let searchInput3 = document.getElementById("search-input3").value.trim();
+    let searchInput4 = document.getElementById("search-input4").value.trim();
+    let searchInput5 = document.getElementById("search-input5").value.trim();
+    let searchInput6 = document.getElementById("search-input6").value.trim();
+    let searchInput7 = document.getElementById("search-input7").value.trim();
+    let searchInput8 = document.getElementById("search-input8").value.trim();
+    let searchInput9 = document.getElementById("search-input9").value.trim();
+    let searchInput10 = document.getElementById("search-input10").value.trim();
+
+// If no definition, use English word + period
+if (definitionInput === "") {
+    definitionInput = `${englishInput}.`;
+}
+
+    let howManySearchInputs = 0;
+    let searchInputArray = [];
+
+    for (let i = 1; i < 11; i++) {
+
+        // let searchInputCheck = `document.getElementById("search-input${i}").value.trim()`;
+        let searchInputData = eval(`document.getElementById("search-input${i}").value.trim()`);
+        if (searchInputData !== "") {
+            howManySearchInputs++;
+            // Add quotes around each array item
+            searchInputData = `\"${searchInputData}\"`;
+            searchInputArray.push(searchInputData);
+        }
+
+        console.log(`how many search inputs: ${howManySearchInputs}. Search Input array: ${searchInputArray}`);
+    }
+let x;
+    if (howManySearchInputs === 0) {
+         x = `\{\"searchInput\":\[\"${scotsInput}\",\"${englishInput}\"\]\,\"scots\":\"${scotsInput}\"\,\"english\":\"${englishInput}\"\,\"definition\":\"${definitionInput}\"\,\"pos\":\"${posInput}\"\}\,`;
+    } else {
+         x = `\{\"searchInput\":\[\"${scotsInput}\",\"${englishInput}\",${searchInputArray}\]\,\"scots\":\"${scotsInput}\"\,\"english\":\"${englishInput}\"\,\"definition\":\"${definitionInput}\"\,\"pos\":\"${posInput}\"\}\,`;
+    }
+
+    if (typeof totalOutput === "undefined") {
+        totalOutput = x;
+    } else {
+        totalOutput = `${totalOutput} ${x}`;
+    }
+
+
+    console.log(`totalOutput: ${totalOutput}`);
+
+    // Restart
+    scotsInput = document.getElementById("scots").value = "";
+    englishInput = document.getElementById("english").value = "";
+    definitionInput = document.getElementById("definition").value = "";
+    posInput = document.getElementById("pos").value = "";
+    searchInput1 = document.getElementById("search-input1").value = "";
+    searchInput2 = document.getElementById("search-input2").value = "";
+    searchInput3 = document.getElementById("search-input3").value = "";
+    searchInput4 = document.getElementById("search-input4").value = "";
+    searchInput5 = document.getElementById("search-input5").value = "";
+    searchInput6 = document.getElementById("search-input6").value = "";
+    searchInput7 = document.getElementById("search-input7").value = "";
+    searchInput8 = document.getElementById("search-input8").value = "";
+    searchInput9 = document.getElementById("search-input9").value = "";
+    searchInput10 = document.getElementById("search-input10").value = "";
+    // Restart
+
+    // Notification
+    document.getElementById("notification").innerText = `Entry submitted`;
+    setTimeout(clearNotification, 1500);
+}
+
+function finished() {
+    let finalOutput = totalOutput.replace(/.$/, "");
+    console.log(finalOutput);
+    document.getElementById("output-box").innerText = finalOutput;
+    navigator.clipboard.writeText(finalOutput);
+
+    // Notification
+    document.getElementById("notification").innerText = `Copied to clipboard`;
+    setTimeout(clearNotification, 1500);
+}
